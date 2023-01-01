552db8b3f01089714e0b2e0a8318f5dd1980b7fd
ort01 <ortutova.kristina@gmail.com>
2023-01-01 18:32:11 +0100

    lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Timer</h1>
+  <button class="play-button" @click="start" :disabled="isPlaying">Play</button>
+  <button class="reset-button" @click="reset">Reset</button>
+  <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from "./components/Block.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Block,
+  },
+  data: () => {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+    },
+    reset() {
+      this.isPlaying = false;
+    },
+  },
+};
 </script>
 
 <style>
@@ -20,7 +37,71 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+.play-button,
+.reset-button {
+  background: none;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+  border: 2px solid rgba(21, 121, 59, 0.493);
+  border-radius: 5px;
+  padding: 10px 20px;
+  margin: 10px 20px;
+  font-size: 18px;
+  font-weight: 600;
+  font-family: inherit;
+  color: #0faf87;
+  box-shadow: 1px 1px 3px 1px rgba(155, 155, 155, 0.507);
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: all 0.5s;
+}
+.play-button:before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 0%;
+  background-color: #0faf87;
+  z-index: -1;
+  border-radius: 0% 0% 50% 50%;
+  transition: all 0.5s;
+}
+
+.play-button:hover,
+.reset-button:hover {
+  color: white;
+}
+.play-button:hover:before,
+.reset-button:hover:before {
+  height: 200%;
+}
+.play-button:disabled {
+  color: rgba(68, 68, 68, 0.301);
+  border: 2px solid rgba(68, 68, 68, 0.301);
+}
+.play-button:disabled:before {
+  background: none;
+}
+
+.reset-button {
+  color: rgb(156, 67, 67);
+  border: 2px solid rgba(170, 53, 53, 0.493);
+}
+.reset-button:before {
+  border-radius: 50% 50% 0% 0%;
+  background-color: rgb(156, 67, 67);
+  content: "";
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 0%;
+  z-index: -1;
+  transition: all 0.5s;
 }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="block" v-if="showBlock">Click me</div>
+</template>
+
+<script>
+export default {
+  name: "Block",
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+    };
+  },
+  // all the functionality inside the mounted() gets executed when the component (which has mounted() inside of its script) is mounted(loaded).
+  mounted() {
+    console.log("component mounted");
+    setTimeout(() => {
+      this.showBlock = true;
+      console.log(this.delay);
+    }, this.delay);
+  },
+  // functionality inside the updated() gets executed when the component (which has updated() inside of its script) is displayed (uptated).
+  updated() {
+    console.log("component updated");
+  },
+  // functionality inside the unmounted() gets executed when the component (which has unounted() inside of its script) is unmounted (is taken out of DOM).
+  unmounted() {
+    console.log("component unmounted");
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.block {
+  width: 300px;
+  border-radius: 20px;
+  box-shadow: 1px 1px 3px 1px rgba(155, 155, 155, 0.507);
+  background: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
